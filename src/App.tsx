2428b4a8d9330a880094fbf7fb2287ab036f33fb
amaanshahsr <amaancreate@gmail.com>
2023-09-03 16:04:27 +0530

    Changes to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Cursor from "./Components/cursor";
 import NavBar from "./Components/NavBar";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import ProductGallery from "./Components/ProductGallery";
 import AboutUs from "./Pages/AboutUs";
@@ -11,18 +11,16 @@ import Footer from "./Components/Footer";
 function App() {
   return (
     <>
-      <Router>
-        <Cursor />
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route index element={<Home />}></Route>
-          <Route path="/home" element={<Home />}></Route>
-          <Route path="/about" element={<AboutUs />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
-          <Route path="/products" element={<ProductGallery />}></Route>
-        </Routes>
-      </Router>
+      <Cursor />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route index element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/about" element={<AboutUs />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/products" element={<ProductGallery />}></Route>
+      </Routes>
       <Footer />
     </>
   );
--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,12 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 function Footer() {
+  const navigate = useNavigate();
   return (
     <footer className="bg-white rounded-lg shadow dark:bg-[#16302d] ">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a
-            href="/"
-            className="flex flex-col sm:flex-row justify-center items-center mb-4 sm:mb-0"
-          >
+          <div className="flex flex-col sm:flex-row justify-center items-center mb-4 sm:mb-0">
             <img
               src="/public/newLogo.png"
               className="h-8 mb-3 sm:mb-0 sm:mr-3"
@@ -15,45 +15,56 @@ function Footer() {
             <span className="self-center uppercase text-2xl font-semibold whitespace-nowrap dark:text-white">
               Saso
             </span>
-          </a>
+          </div>
           <ul className="flex flex-wrap justify-center  items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
-              <a
-                href="/home"
+              <div
+                onClick={() => {
+                  navigate("/home");
+                }}
                 className="mr-4 hover:underline md:mr-6 interactable link "
               >
                 Home
-              </a>
+              </div>
             </li>
             <li>
-              <a
-                href="/about"
+              <div
+                onClick={() => {
+                  navigate("/about");
+                }}
                 className="mr-4 hover:underline md:mr-6 interactable link"
               >
                 About
-              </a>
+              </div>
             </li>
             <li>
-              <a
-                href="/products"
+              <div
+                onClick={() => {
+                  navigate("/products");
+                }}
                 className="mr-4 hover:underline md:mr-6 interactable link "
               >
                 Products
-              </a>
+              </div>
             </li>
             <li>
-              <a href="/contact" className="hover:underline interactable link">
+              <div
+                onClick={() => {
+                  navigate("/contact");
+                }}
+                className="hover:underline interactable link"
+              >
                 Contact
-              </a>
+              </div>
             </li>
           </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm w-max mx-auto  text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
-          <a href="/" className="hover:underline">
+          <div href="/" className="hover:underline">
             Saso
-          </a>
+          </div>
           . All Rights Reserved.
         </span>
       </div>
